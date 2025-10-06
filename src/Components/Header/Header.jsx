c8ed482df578b750bfbb5eb9ff82326e950d5482
Muhammad Zobayer <devzobayer@hotmail.com>
2025-10-07 00:26:16 +0600

    fix bug

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -26,7 +26,13 @@ const Header = () => {
   <div className="logo-img flex items-start">
     <Link className='font-semibold zmbook nunito-sans'>
       <img className='max-w-28 hidden md:block' src={Images.Logo} alt="" />
-      <img className='max-w-12 block md:hidden' src={Images.LogoTwo} alt="" />
+        <img
+        className={`max-w-12 transform transition-all duration-300 ease-in-out
+          ${searchOpen ? 'opacity-0 -translate-x-5  w-0 overflow-hidden scale-95' : 'opacity-100 translate-x-0 w-12 scale-100'}
+          md:hidden`}
+        src={Images.LogoTwo}
+        alt="Logo"
+      />
     </Link>
   </div>
 
