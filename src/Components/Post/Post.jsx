09f876cf37f3e9b3c4d4ad82148c6500d35d3949
Muhammad Zobayer <devzobayer@hotmail.com>
2025-10-11 22:25:30 +0600

    add post

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Images from '../../utilits/Images/Images'
+import Profile from '../../utilits/Profile'
+
+const Post = () => {
+  return (
+   <div className="w-full bg-slate-50 rounded-2xl mt-2 p-4 ">
+            <div className="post-card flex">
+              <Profile/>
+              <input type="text" 
+              className='w-full ml-4 input border-gray-300 bg-white rounded-full font-semibold'
+              placeholder="What's on your mind, Muhammad?"
+               />
+            </div>
+            <div className="border-b-1 border-gray-300 my-4"></div>
+
+           <div className="grid grid-cols-3 gap-2 text-center">
+        <div className="activity flex justify-center items-center hover:bg-gray-100 rounded-lg p-1 cursor-pointer transition">
+          <img className="w-7" src={Images.Live} alt="" />
+          <h5 className="ml-1 font-semibold text-gray-600 text-sm">Live Video</h5>
+        </div>
+
+        <div className="activity flex justify-center items-center hover:bg-gray-100 rounded-lg p-1  cursor-pointer transition">
+          <img className="w-7" src={Images.Photo} alt="" />
+          <h5 className="ml-1 font-semibold text-gray-600 text-sm">Photo/Video</h5>
+        </div>
+
+        <div className="activity flex justify-center items-center hover:bg-gray-100 rounded-lg p-1 cursor-pointer transition">
+          <img className="w-7" src={Images.Happy} alt="" />
+          <h5 className="ml-1 font-semibold text-gray-600 text-sm">Feelings</h5>
+        </div>
+      </div>
+
+          </div>
+  )
+}
+
+export default Post
--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router'
 import Images from '../utilits/Images/Images'
 import Test from '../utilits/test'
 import Profile from '../utilits/Profile'
+import Post from '../Components/Post/Post'
 
 const HomePage = () => {
   return (
@@ -16,35 +17,7 @@ const HomePage = () => {
 
         {/* Center (post) */}
         <div className="post text-center ">
-        
-          <div className="w-full bg-slate-50 rounded-2xl mt-2 p-4 ">
-            <div className="post-card flex">
-              <Profile/>
-              <input type="text" 
-              className='w-full ml-4 input border-gray-300 bg-white rounded-full font-semibold'
-              placeholder="What's on your mind, Muhammad?"
-               />
-            </div>
-            <div className="border-b-1 border-gray-300 my-4"></div>
-            <div className="m-auto">
-            <div className="grid grid-cols-3 gap-2 ">
-               <div className="activity flex items-center">
-                 <img className='w-7' src={Images.Live} alt="" />
-                 <h5 className='ml-1 font-semibold text-gray-500'>Live Video</h5>
-               </div>
-               <div className="activity flex items-center">
-                 <img className='w-7' src={Images.Photo} alt="" />
-                 <h5 className='ml-1 font-semibold text-gray-500'>Photo/Video</h5>
-               </div>
-               <div className="activity flex items-center">
-                 <img className='w-7' src={Images.Happy} alt="" />
-                 <h5 className='ml-1 font-semibold text-gray-500'>Fellings</h5>
-               </div>
-               
-            </div>
-            </div>
-          </div>
-
+         <Post/>
         </div>
 
         {/* Right side (chat) */}
