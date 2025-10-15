15a16a04c21c3602e061ee874f6d36f112be1b55
Muhammad Zobayer <devzobayer@hotmail.com>
2025-10-15 23:03:21 +0600

    bug fix

--- src/Components/MobileMenu/MobileMenu.jsx
+++ src/Components/MobileMenu/MobileMenu.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router'
 
 const MobileMenu = () => {
   return (
-     <div className={`flex items-center  space-x-8  md:hidden mr-4`}>
+     <div className={`flex items-center  space-x-8  md:hidden mr-7 mt-2`}>
              <Link><img className='max-w-8' src={Images.Home} alt="" /></Link>
-             <Link><img className='max-w-8' src={Images.Videos} alt="" /></Link>
-             <Link><img className='max-w-8' src={Images.Notify} alt="" /></Link>
+             <Link><img className='max-w-7' src={Images.Videos} alt="" /></Link>
+             <Link><img className='max-w-7' src={Images.Notify} alt="" /></Link>
               </div>
   )
   
